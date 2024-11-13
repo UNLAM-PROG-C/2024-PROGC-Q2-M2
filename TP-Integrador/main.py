557321731d3e843852c41947ba3d4333f289f02c
martinezbrenda <111657093+martinezbrenda@users.noreply.github.com>
2024-11-13 02:53:43 -0300

    score

--- TP-Integrador/main.py
+++ TP-Integrador/main.py
@@ -8,12 +8,12 @@ from jugador import Jugador
 from hilos import GeneradorFormas, MovimientoFormas
 from forma import dibujar_forma
 from menu import mostrar_portada, mostrar_menu, mostrar_seleccion_personaje, mostrar_pantalla_fin
-from utils import  imagen_fondo, filtro_blanco_negro
+from utils import  imagen_fondo
 from globals import ANCHO, ALTO, NEGRO, reloj, pantalla, velocidad_jugador, cola_formas, lock_formas
 
 pygame.init()
 
-def ejecutar_juego(nombre_jugador):
+def inicializar_juego():
     """Función principal para ejecutar el flujo del juego."""
     global tiempo_inicial, ejecutando, stop_event, lock_formas, cola_formas, reloj
 
@@ -24,7 +24,6 @@ def ejecutar_juego(nombre_jugador):
     ejecutando = True
     cola_formas = Queue()
     stop_event = threading.Event()
-
 
 def iniciar_hilos():
     """Inicia los hilos para generar y mover formas."""
@@ -85,11 +84,11 @@ def mostrar_hud():
     pantalla.blit(texto_puntaje, (10, 10))
     pantalla.blit(texto_vidas, (10, 50))
 
+
 def verificar_fin_del_juego():
     """Verifica si el jugador ha perdido todas las vidas y muestra el mensaje final."""
     global ejecutando
     if globals.vidas <= 0:
-        filtro_blanco_negro(pantalla)
         fuente_game_over = pygame.font.SysFont(None, 72)
         texto_game_over = fuente_game_over.render("¡Juego Terminado!", True, NEGRO)
         pantalla.blit(texto_game_over, (ANCHO // 2 - texto_game_over.get_width() // 2, ALTO // 2))
@@ -108,7 +107,7 @@ def esperar_hilos(hilo_generador, hilo_movedor):
     
 
 
-def ejecutar_juego():
+def ejecutar_juego(nombre_jugador):
     """Función principal para ejecutar el flujo del juego."""
     inicializar_juego()
     hilo_generador, hilo_movedor = iniciar_hilos()
@@ -136,14 +135,13 @@ def ejecutar_juego():
         pygame.display.flip()
 
     esperar_hilos(hilo_generador, hilo_movedor)
-
-    # Mostrar la pantalla de fin del juego y tomar la acción correspondiente
+    
     return mostrar_pantalla_fin()
 
 # Inicia el flujo principal del juego
 if __name__ == "__main__":
-    nombre_jugador = inicio()
     mostrar_portada()
+    nombre_jugador = inicio()
     
     while True:
         # Seleccionar el personaje del jugador
